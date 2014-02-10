f2e5047fab48cad100f7b1c0d4f080ee505cf4c4
zedwarth <zedwarth@gmail.com>
2014-02-10 14:01:57 -0600

    Move Pizza class to be below topping class.  Pizza now calls a default toping of cheese as the default topping and passes the test.

--- pizza.rb
+++ pizza.rb
@@ -1,17 +1,3 @@
-class Pizza
-	attr_accessor :toppings
-
-	# Instantiate a new pizza.
-	#
-	# toppings - An array containing Topping objects.
-	#
-	# Returns a new Pizza object.
-
-	def initialize(toppings)
-		@toppings = toppings
-	end
-end
-
 class Topping
 	attr_accessor :name, :vegetarian
 
@@ -25,3 +11,21 @@ class Topping
 		@vegetarian = vegetarian
 	end
 end
+
+class Pizza
+	attr_accessor :toppings
+
+	# Instantiate a new pizza.
+	#
+	# toppings - An array containing Topping objects.
+	#
+	# Returns a new Pizza object.
+	
+	@@cheese = Topping.new('cheese', vegetarian: true)
+
+	def initialize(toppings = [@@cheese])
+		@toppings = toppings
+	end
+end
+
+
