208a8a502d88ccca3eac63860f49cbb07b04caf9
zedwarth <zedwarth@gmail.com>
2014-02-10 11:08:59 -0600

    Pass "sets the name of the topping" test.

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,14 @@ class Pizza
 end
 
 class Topping
+	attr_accessor :name
+
+	# Instantiate a new topping.
+	#
+	# name - The string name of the topping.
+	#
+	# Returns a new Topping object.
+	def initialize(name)
+		@name = name
+	end
 end
